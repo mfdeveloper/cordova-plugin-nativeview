2d4383e5eaacb29d73e4395440b4457aefbed4f0
Michel Felipe <mfelipeof@gmail.com>
2018-10-02 11:58:16 -0300

    Refactors in IOS Objective-C plugin, to allow many scenarios to instantiate a ViewController from native

--- src/ios/CDVNativeView.h
+++ src/ios/CDVNativeView.h
@@ -1,14 +1,24 @@
 //
 //  CDVNativeView.h
-//  IRPF
 //
 //  Created by Michel Felipe on 05/09/17.
 //
 //
 
+// Reference: https://stackoverflow.com/questions/7017281/performselector-may-cause-a-leak-because-its-selector-is-unknown
+#define SuppressPerformSelectorLeakWarning(Stuff) \
+    do { \
+        _Pragma("clang diagnostic push") \
+        _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+        Stuff; \
+        _Pragma("clang diagnostic pop") \
+    } while (0)
+
 #import <Cordova/CDV.h>
 
 @interface CDVNativeView : CDVPlugin
+
+@property (strong, nonatomic) NSDictionary *resultExceptions;
 
 - (void)show:(CDVInvokedUrlCommand*)command;
 
